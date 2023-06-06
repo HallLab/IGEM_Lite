09b21166d7d6d0ef6636c0abf243fdca04a9e70f
Andre Rico <97684721+AndreRicoPSU@users.noreply.github.com>
2023-06-06 08:37:15 -0400

    Add exe_pairwise function

--- igem/epc/clarite/analyze/__init__.py
+++ igem/epc/clarite/analyze/__init__.py
@@ -4,10 +4,12 @@ from .analyze import (  # noqa E501
     ewas,
     interaction_study,
 )
+from .interaction import exe_pairwise
 
 __all__ = [
     "association_study",
     "ewas",
     "interaction_study",
     "add_corrected_pvalues",
+    "exe_pairwise",
 ]
